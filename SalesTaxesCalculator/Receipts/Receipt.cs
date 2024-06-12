0aefefbfb359e3ac87c458397284ed5f4d79b2f7
Suzan Letjobana Masite <suzan@istratgo.com>
2024-06-12 10:37:22 +0200

    Added the receipt class

---
+++ SalesTaxesCalculator/Receipts/Receipt.cs
@@ -0,0 +1,39 @@
+﻿using SalesTaxesCalculator.Models;
+using SalesTaxesCalculator.Services;
+
+namespace SalesTaxesCalculator.Receipts
+{
+    public class Receipt
+    {
+        private List<Item> _items;
+
+        public Receipt()
+        {
+            _items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void PrintReceipt()
+        {
+            decimal totalTax = 0m;
+            decimal totalPrice = 0m;
+
+            foreach (var item in _items)
+            {
+                decimal itemTax = TaxCalculator.CalculateTax(item);
+                decimal itemTotalPrice = item.Price + itemTax;
+                totalTax += itemTax;
+                totalPrice += itemTotalPrice;
+
+                Console.WriteLine($"{item.Name}: {itemTotalPrice:F2}");
+            }
+
+            Console.WriteLine($"Sales Taxes: {totalTax:F2}");
+            Console.WriteLine($"Total: {totalPrice:F2}");
+        }
+    }
+}
